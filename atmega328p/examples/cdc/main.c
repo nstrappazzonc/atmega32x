ecacbcd10c7966136a13a496d56ee5d52e94cb45
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2022-11-24 21:42:14 +0100

    move cdc logic into function

--- atmega328p/examples/cdc/main.c
+++ atmega328p/examples/cdc/main.c
@@ -4,34 +4,43 @@
 #define USART_BAUDRATE 9600
 #define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
+static void USARTInit();
+static void USARTSend(uint8_t);
+static void USARTSendString(char* string);
+
 int main(void) {
-    UBRR0H = (uint8_t)(UBRR_VALUE >> 8);
-    UBRR0L = (uint8_t)(UBRR_VALUE);
+    USARTInit();
+
+    while(1){
+        USARTSendString("Hello from ATmega328p with CH340C\n\r");
+        _delay_ms(1000);
+    }
+
+    return 0;
+}
+
+static void USARTInit() {
+    UBRR0H  = (uint8_t)(UBRR_VALUE >> 8);
+    UBRR0L  = (uint8_t)(UBRR_VALUE);
     UCSR0B |= (1<<RXEN0);   // Turn on receiver.
     UCSR0B |= (1<<TXEN0);   // Turn on transmitter.
     UCSR0C |= (0<<UMSEL00); // Set asynchronous mode.
     UCSR0C |= (0<<UPM00);   // Disable parity mode.
     UCSR0C |= (0<<USBS0);   // Set 8 bits frame data.
     UCSR0C |= (3<<UCSZ00);  // Set 1 stop bit.
+}
 
-    int i = 0;
-    unsigned char data[] = "Hello from ATmega328p with CH340C";
+static void USARTSend(uint8_t data){
+    // Wait for empty transmit buffer.
+    while(!(UCSR0A & (1<<UDRE0)));
 
-    while(1){
-        i = 0;
-        while(data[i] != 0){
-            while (!(UCSR0A & (1<<UDRE0)));
-            UDR0 = data[i];
-            i++;
-        }
+    // Put data into buffer, sends the data.
+    UDR0 = data;
+}
 
-        while (!(UCSR0A & (1<<UDRE0)));
-        UDR0 = '\n';
-        while (!(UCSR0A & (1<<UDRE0)));
-        UDR0 = '\r';
-
-        _delay_ms(1000);
+static void USARTSendString(char* strptr){
+    // Sends the characters from the string one at a time.
+    while(*strptr != 0x00) {
+        USARTSend(*strptr++);
     }
-
-    return 0;
 }
