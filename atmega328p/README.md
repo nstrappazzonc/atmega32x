26ccd6f7664af3da8f13d52193517e9f9d5dbdbc
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2022-11-26 19:46:50 +0100

    add more examples; cdc rx and push.

--- atmega328p/README.md
+++ atmega328p/README.md
@@ -1,3 +1,11 @@
 # ATmega328P
 
 ![](https://github.com/nstrappazzonc/atmega32x/blob/main/atmega328p/assets/protoboard_cdc.jpg?raw=true)
+
+Examples:
+
+- [Led blink.](https://github.com/nstrappazzonc/atmega32x/tree/main/atmega328p/examples/led/normal)
+- [Push button and turn ON/OFF led.](https://github.com/nstrappazzonc/atmega32x/tree/main/atmega328p/examples/push)
+- [Fade, use PWM.](https://github.com/nstrappazzonc/atmega32x/tree/main/atmega328p/examples/led/fade)
+- [CDC, read and write via terminal.](https://github.com/nstrappazzonc/atmega32x/tree/main/atmega328p/examples/cdc)
+- ADC, read analog values from LDR.
---
+++ atmega328p/examples/cdc/rx/main.c
@@ -0,0 +1,56 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define USART_BAUDRATE 9600
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+static void USARTInit();
+static void USARTSend(uint8_t);
+static void USARTSendString(char* string);
+static unsigned char USARTRead(void);
+
+int main(void) {
+    USARTInit();
+    USARTSendString("Write anything:\n\r");
+
+    while(1){
+        USARTSend(USARTRead());
+        _delay_ms(1000);
+    }
+
+    return 0;
+}
+
+static void USARTInit() {
+    UBRR0H  = (uint8_t)(UBRR_VALUE >> 8);
+    UBRR0L  = (uint8_t)(UBRR_VALUE);
+    UCSR0B |= (1<<RXEN0);   // Turn on receiver.
+    UCSR0B |= (1<<TXEN0);   // Turn on transmitter.
+    UCSR0C |= (0<<UMSEL00); // Set asynchronous mode.
+    UCSR0C |= (0<<UPM00);   // Disable parity mode.
+    UCSR0C |= (0<<USBS0);   // Set 8 bits frame data.
+    UCSR0C |= (3<<UCSZ00);  // Set 1 stop bit.
+}
+
+static void USARTSend(uint8_t data){
+    // Wait for empty transmit buffer.
+    while(!(UCSR0A & (1<<UDRE0)));
+
+    // Put data into buffer, sends the data.
+    UDR0 = data;
+}
+
+static void USARTSendString(char* strptr){
+    // Sends the characters from the string one at a time.
+    while(*strptr != 0x00) {
+        USARTSend(*strptr++);
+    }
+}
+
+static unsigned char USARTRead(void) {
+    // Wait for data to be received.
+    while (!(UCSR0A & (1<<RXC0)));
+
+    // Get and return received data from buffer.
+    return UDR0;
+}
