22b26c9367503843083d0504b44bb9b1792e0a69
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2022-12-15 18:27:34 +0100

    first version of adc

--- atmega328p/examples/adc/main.c
+++ atmega328p/examples/adc/main.c
@@ -1,6 +1,48 @@
-int main(void) {
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+#define USART_BAUDRATE 9600
+#define UBRR_VALUE ((F_CPU / (USART_BAUDRATE * 16UL)) - 1)
+
+static int usart_putchar(char, FILE*);
+
+FILE usart_stdout = FDEV_SETUP_STREAM(usart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+int main(void)
+{
+    // Cofigure CDC:
+    UBRR0H  = (unsigned char)(UBRR_VALUE >> 8);
+    UBRR0L  = (unsigned char)(UBRR_VALUE);
+    UCSR0B |= (1<<RXEN0);   // Turn on receiver.
+    UCSR0B |= (1<<TXEN0);   // Turn on transmitter.
+    UCSR0C |= (0<<UMSEL00); // Set asynchronous mode.
+    UCSR0C |= (0<<UPM00);   // Disable parity mode.
+    UCSR0C |= (0<<USBS0);   // Set 8 bits frame data.
+    UCSR0C |= (3<<UCSZ00);  // Set 1 stop bit.
+
+    stdout = &usart_stdout;
+
+    // Configure ADC:
+    
+
+    
+    double pi = 3.14159;
+
     while (1) {
+        printf("Value: %f\n\r", pi);
+        _delay_ms(1000);
     }
 
+    return 0;
+}
+
+static int usart_putchar(char c, FILE *stream)
+{
+    // Wait until data register is empty
+    while((UCSR0A & (1 << UDRE0)) == 0) {;}
+    // Load transmit register with new data
+    UDR0 = c;
+    _delay_ms(1);
     return 0;
 }
